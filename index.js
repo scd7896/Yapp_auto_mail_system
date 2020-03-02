c6cdb6dd24e828fafa8b716a235bac42753030a5
scd7896 <scd1212@naver.com>
2020-03-02 22:59:56 +0900

    불필요한 커밋 지우기, 한페이지에서 다 보이게하기 위한 ejs 삭제, 엑셀불러오기와 socket통신으로 실시간 프론트에 데이터 보내기, 리드미 투두리스트 증가

--- index.js
+++ index.js
@@ -1,51 +1,105 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
-const fs = require('fs');
-const http = require('http')
-const server = http.createServer(app)
-const io = require('socket.io').listen(server)
 dotenv.config();
-const apiKey = process.env.API_KEY
-var domain = process.env.DOMAIN;
-var mailgun = require('mailgun-js')({apiKey: apiKey, domain: domain, host: "api.eu.mailgun.net"});
-app.use('/static',express.static(__dirname+'/dist'));
-
-
-
-app.set('view engine', 'ejs');
-const allUsers = [];
-const completedUsers = [];
-
-io.sockets.on('connection', function(socket){
-    io.emit('list-add', completedUsers);
+const app = express();
+const http = require('http')
+const server = http.Server(app)
+const io = require('socket.io')(server, {
+    cookie: false
 })
-
-const sendUserResult = (user) =>{
-    io.emit('list-add', user);
+const cors = require('cors');
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use(cors({
+    origin : true
+}))
+const fs = require('fs'); 
+const XLSX = require('xlsx'); //엑셀파일 읽어오는 모듈
+const allSheets = XLSX.readFile(__dirname + "/public/nodeexcel.xlsx") // 엑셀의 전체 시트를 읽자
+const resultSheet = allSheets.Sheets['result'] // 그중에서 result 시트만 읽고
+const allUsers = []; //보낼 전체유저 
+const allMemBerCount = process.env.ALL_MEMBER_COUNT; //env에서 정의한 숫자
+for(let i = 2 ; i<=allMemBerCount; i++){
+    //시트 행 번호에 따른 정보
+    /**
+     *  H: 합격여부
+     *  A: 이메일
+     *  C: 이름
+     */
+    //합격 여부 확인해서 필요한 데이터 재가공
+    const isPass = resultSheet[`H${i}`] !== undefined && resultSheet[`H${i}`].v == '합격'
+    const oneUser = {
+        email: resultSheet[`A${i}`].v,
+        name: resultSheet[`C${i}`].v,
+        isPass: isPass
+    }
+    allUsers.push(oneUser)
 }
 
-app.get('/', (req,res)=>{
+
+const apiKey = process.env.API_KEY // mailgun apiKey
+var domain = process.env.DOMAIN; // mailgun domain
+var mailgun = require('mailgun-js')({apiKey: apiKey, domain: domain, host: "api.eu.mailgun.net"}); //mailgun 모듈
+
+app.use('/static',express.static(__dirname+'/dist')); // react 쓰기위한 것
+app.use(express.static(__dirname + '/'));
+app.set('view engine', 'ejs'); // .env를 사용하기 위한 ejs
+
+
+
+io.on('connection', function(socket){
+    console.log('연결됨');
+    io.emit('list-add', null)
+})
+
+const sendUserResult = (user, isError) =>{ // 메일 보내는 것 성공여부에 관계없이 결과를 프론트에 던져준다.
+    const toClient = {
+        ...user,
+        isError
+    }
+    io.emit('list-add', toClient);
+}
+const createUserData = (user) => {
+    return {
+        from: process.env.EMAIL,
+        to: 'yappsend@gmail.com',
+        subject: `${user.name}의 결과물`,
+        html: `
+        <p>user의 이메일 : ${user.email}</p>
+        <p>user의 이름 : ${user.name} </p>
+        <p>합격 여부 : ${user.isPass} </p>
+        `
+    };
+}
+
+app.get('/', (req,res)=>{ // 단순 인덱스 페이지를 열어주는 web 라우터
     return res.render('index')
 })
-app.get('/usersTest',(req,res)=>{
-    return res.status(200).send(allUsers)
+
+app.get('/sendmail', (req, res)=>{
+    console.log('메일 전송 시작')
+    for(let i = 0; i < 4; i++){
+        const data = createUserData(allUsers[i]);
+        const oneUser = allUsers[i];
+        setTimeout(()=>{
+            mailgun.messages().send(data, function (error, body) {
+                if(error){
+                    sendUserResult(oneUser, true);
+                    return;
+                }else{
+                    sendUserResult(oneUser, false);
+                    return;
+                }
+            });
+        }, i * 1000)
+    }
+    return res.status(200).send(process.env.ALL_MEMBER_COUNT);
 })
-app.get('/userlist', (req, res)=>{
-    return res.render('listcheck')
+
+app.post("/resendmail", (req, res)=>{
+    const list = req.body.list
+    console.log(list);
+    return res.status(200).send('good')
 })
-app.get("/mail/shot", (req, res)=>{
-    var data = {
-        from: process.env.EMAIL,
-        to: 'scd1212@naver.com',
-        subject: 'Hello',
-        text: 'Testing some Mailgun awesomness!'
-    };
-    mailgun.messages().send(data, function (error, body) {
-            
-        console.log('에러',error)
-        
-        console.log('바디',body);
-    });
-})
-app.listen(9170 , () => console.log("서버 진행중 9170"));
+
+server.listen(9170 , () => console.log("서버 진행중 9170"));
